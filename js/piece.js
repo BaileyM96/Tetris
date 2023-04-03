b218d527d8409b246dafd299ef06465a74bf60c6
Bailey Mejia <baileymejia@Baileys-MBP.lan>
2023-04-02 21:51:22 -0700

    Added file for pieces to create the pieces on screen.

---
+++ js/piece.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById('canvas');
+// Set up canvas context to render the graphics
+let ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(x, y, width, height);

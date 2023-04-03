3e828893a75a089e8b2b028cdc44499034b5df28
Bailey Mejia <baileymejia@Baileys-MacBook-Pro.local>
2023-04-03 12:50:25 -0700

    Added key movements to main.js, and got the piece to display.

--- js/main.js
+++ js/main.js
@@ -9,8 +9,23 @@ ctx.canvas.height = COLS * BLOCK_SIZE;
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 let board = new Board();
+
+const moves = {
+    [KEY.LEFT]: p => ({ ...p, x: p.x - 1}),
+    [KEY.RIGHT]: p => ({ ...p, x: p.x + 1}),
+    [KEY.DOWN]: p => ({ ...p, y: p.y + 1})
+};
 //Set up the play button function
 function play() {
     board.reset();
+    let piece = new Piece(ctx);
+    piece.draw();
+    board.piece = piece;
     console.table(board.grid);
 };
+
+function addEventListener() {
+    document.addEventListener('keydown', event => {
+        if(moves[event.keyCode] (board.piece))
+    })
+}
--- js/piece.js
+++ js/piece.js
@@ -15,8 +15,15 @@ class Piece {
         this.x = 3;
         this.y = 0;
     }
+    draw() {
+        this.ctx.fillstyle = this.color;
+        this.shape.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value > 0) {
+                    this.ctx.fillRect(this.x + x, this.y 
+                        + y, 1, 1);
+                }
+            });
+        });
+    }
 };
-
-draw() {
-    this.ctx.fillstyle = this.color;
-}
